ead024897eed367d041f88a519257d63833d250c
Andrew Greenwood <andrewgreenwoo@gmail.com>
2014-04-07 00:59:59 -0500

    trying to merge all the work done together

--- WaterMinerTechDemo/Assets/Scripts/GrassController.cs
+++ WaterMinerTechDemo/Assets/Scripts/GrassController.cs
@@ -1,25 +1,74 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public class GrassController : MonoBehaviour {
 
-	private Animator animator;
+	private Animator playerAnimator;
+	public Sprite[] sprites;
+	public float framesPerSecond;
 	
+	
+	private SpriteRenderer spriteRenderer;
+	private bool touched = false;
+	private bool grounded = false;
+	private Stopwatch timer;
+	//private float groundRadius = 0.2f;
+	private GameObject player; // player object for moving 
+
 	// Use this for initialization
 	void Start () {
-		animator = this.GetComponent<Animator>();
+
+		player = GameObject.FindWithTag ("Player");
+		playerAnimator = player.GetComponent<Animator>();
+		timer = new Stopwatch ();
+		spriteRenderer = renderer as SpriteRenderer;
+		//UnityEngine.Debug.Log(spriteRenderer.bounds.extents.x);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () { 
+
+
+	}
+
+	// FixedUpdate is run at specific time intervals.
+	void FixedUpdate () {
+		if (player.transform.position.x < transform.position.x + (spriteRenderer.bounds.extents.x)
+		&& player.transform.position.x > transform.position.x - (spriteRenderer.bounds.extents.x)
+		    && player.transform.position.y < transform.position.y + spriteRenderer.bounds.extents.y * 2) {
+			grounded = true;
+		} else {
+			grounded = false;
+			touched = false;
+			if(timer.IsRunning){
+				timer.Stop();
+				timer.Reset();
+				spriteRenderer.sprite = sprites [0];
+			}
+		}
+
+		if(grounded){
+			if (touched) {
+				int index = (int)(0.01f * timer.ElapsedMilliseconds);
+				//UnityEngine.Debug.Log(boxCol);
+				if(index == sprites.Length){
+					Destroy(gameObject);
+				}else if(index < sprites.Length){
+					spriteRenderer.sprite = sprites [index];
+				}
+			}
+		}
 	}
 	
 	void OnMouseDown()
 	{
-		animator.SetInteger("Dig", 1);
-		//animator.speed = 1.0f;
-		//animator.Play();
-		Destroy(gameObject);
+		if (Mathf.Abs (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - character.rigidbody2D.transform.position.x) < 100 ||
+		    Mathf.Abs (Camera.main.ScreenToWorldPoint(Input.mousePosition).y - character.rigidbody2D.transform.position.y) < 100) {
+			playerAnimator.Play("Swing");
+			touched = true;
+			if(grounded){
+			timer.Start();
+		}
 	}
 }
