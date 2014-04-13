121f1db4880352b59703319323aad413f8e43287
James Boullion <jboullion83@gmail.com>
2014-04-13 13:37:26 -0500

    Added Menu Screen and Audio sources.

---
+++ WaterMinerTechDemo/Assets/Scripts/StartButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	public float delay;
+
+	private bool starting = false;
+	private float startTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (starting && Time.time > startTime) {
+			Application.LoadLevel("level_1");
+		}
+	}
+
+	void OnMouseDown(){
+		starting = true;
+		startTime = Time.time + delay;
+		audio.Play();
+	}
+}
