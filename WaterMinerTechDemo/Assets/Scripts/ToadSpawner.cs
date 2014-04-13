4e5c042197e800224b8df977ce08a3df047ac033
James Boullion <jboullion83@gmail.com>
2014-04-13 14:32:23 -0500

    Added a toad spawn. Minor changes in some prefabs.

---
+++ WaterMinerTechDemo/Assets/Scripts/ToadSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToadSpawner : MonoBehaviour {
+
+	public float delay;
+	public GameObject toad;
+
+	private bool starting = false;
+	private float startTime = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time + delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > startTime) {
+			Instantiate(toad, transform.position, transform.rotation);
+			startTime = Time.time + delay;
+			audio.Play();
+		}
+	}
+	
+}
